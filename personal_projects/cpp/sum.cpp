183fe02275f5dda6eb4d5d8a05f975b04382c061
GR130398 <gabriele.ribolla@mail.polimi.it>
2023-01-27 17:36:09 +0100

    update sum

---
+++ personal_projects/cpp/sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "math.h"
+#include <istream>
+
+using namespace std;
+
+int sum(int a, int b){
+    return a + b;
+}
+
+
+int main() {
+    int a,b;
+    
+    cout <<"inserisci un numero"<<endl;
+    cin >> a;
+    cout << "inserici un altro numero "<<endl ;
+    cin>> b;
+    cout<< sum(a,b);
+
+    return 0;
+}
+
+
