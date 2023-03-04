84b122b67b1ce5a7347b4aa7bf54cf1d1c1d4699
GR130398 <gabriele.ribolla@mail.polimi.it>
2023-03-04 12:11:35 +0100

    update

---
+++ personal_projects/cpp/pointers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+
+int main(){
+    //std::cout<<"hello, world!\a"<<std::endl;
+    int ciao = 3;
+   // foo = &ciao;
+   int ** foo;
+   int * fooo;
+   int myArray[40];
+   myArray[0] = 0;
+   myArray[1] = 1;
+   int  * fo;
+   fo = myArray;
+  
+   fooo = &ciao;
+    foo = &fooo;
+    std::cout<<&ciao<<std::endl;
+    std::cout<<foo<<std::endl;
+    std::cout<<ciao<<std::endl;
+    **foo = 10;
+    std::cout<<ciao<<std::endl;
+    std::cout<<&ciao + 1 <<std::endl;
+    std::cout<<fooo<<std::endl;
+    std::cout<<*(fo+1)<<std::endl;
+    std::cout<<myArray[0]<<std::endl;
+}
